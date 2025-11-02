ae2a7461a9944ea9c434afa28f389d344d10607a
iamkunalgoswami <kunalgoswami99@gmail.com>
2025-11-02 23:55:47 +0530

    LearnDataJPA - Added the Student Entity.

---
+++ src/main/java/com/selenophile/learnspringdatajpa/Student.java
@@ -0,0 +1,97 @@
+package com.selenophile.learnspringdatajpa;
+
+import javax.persistence.*;
+
+@Entity(name = "Student")
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1,
+            initialValue = 500000
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT",
+            length = 255
+    )
+    private String firstName;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT",
+            length = 255
+    )
+    private String lastName;
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT",
+            unique = true,
+            length = 255
+    )
+    private String email;
+    @Column(
+            name = "age",
+            nullable = false
+    )
+    private int age;
+
+    public Student(Long id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
